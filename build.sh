b4c04680fd248a2a9601389ead6d4a73491e7138
greymd <greengregson@gmail.com>
2019-03-12 22:04:59 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ue
-git clone https://github.com/egison/egison.git "$HOME"/egison
-cd "${HOME}"/egison
-cabal update
-cabal install --only-dependencies
-cabal configure --datadir=/usr/lib --datasubdir=egison
-cabal build
+{
+  git clone https://github.com/egison/egison.git "$HOME"/egison
+  cd "${HOME}"/egison
+  cabal update
+  cabal install --only-dependencies
+  cabal configure --datadir=/usr/lib --datasubdir=egison
+  cabal build
+} >&2
 cat "${HOME}/egison/dist/build/egison/egison"
