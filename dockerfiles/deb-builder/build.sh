4d7b0d5eba29a4e9715975a39fa9c64027a5bced
greymd <greengregson@gmail.com>
2019-03-16 23:06:59 +0000

    Complete deb-builder

--- dockerfiles/deb-builder/build.sh
+++ dockerfiles/deb-builder/build.sh
@@ -4,12 +4,17 @@ VERSION="$1"
 REPO="greymd/rpm-egison"
 {
   cd /tmp
-  wget "https://github.com/${REPO}/releases/download/${VERSION}/egison_linux_$(uname -m)_${VERSION}.tar.gz"
-  tar zxvf "egison_linux_$(uname -m)_${VERSION}.tar.gz"
-  cp -rf "egison-${VERSION}"/bin deb-template/bin
-  cp -rf "egison-${VERSION}"/lib deb-template/lib
-  bash changelog.sh
-  mv deb-template "egison-${VERSION}"
-  cd "egison-${VERSION}"
+  (
+    mkdir /tmp/extract
+    cd /tmp/extract
+    wget "https://github.com/${REPO}/releases/download/${VERSION}/egison_linux_$(uname -m)_${VERSION}.tar.gz"
+    tar zxvf "egison_linux_$(uname -m)_${VERSION}.tar.gz"
+  )
+  cp -rf "/tmp/extract/egison-${VERSION}"/{bin,lib} /tmp/deb-template/
+  rm -rf "/tmp/extract"
+  bash -x /tmp/changelog.sh
+  mv /tmp/deb-template "/tmp/egison-${VERSION}"
+  cd "/tmp/egison-${VERSION}"
   debuild -sd <<<y
 } >&2
+cat /tmp/egison*.deb
