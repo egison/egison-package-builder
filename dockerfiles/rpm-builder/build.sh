f3f55496851aa43fd07d7387630c4ee380ca9506
greymd <greengregson@gmail.com>
2019-03-16 19:46:37 +0000

    set __libdir /usr/lib

--- dockerfiles/rpm-builder/build.sh
+++ dockerfiles/rpm-builder/build.sh
@@ -4,7 +4,7 @@ set -u
 VERSION="$1"
 {
   sed -i "s/@@@VERSION@@@/${VERSION}/" /tmp/egison.spec
-  rpmbuild --undefine=_disable_source_fetch -ba /tmp/egison.spec
+  rpmbuild --undefine=_disable_source_fetch --define="_libdir /usr/lib" -ba /tmp/egison.spec
 } >&2
 # It is expectec to be single file.
 cat "${HOME}/rpmbuild/RPMS/$(uname -m)"/egison*.rpm
