602e803a2c5b7d3a1ccef5883d8d4ae9a63d72b5
greymd <greengregson@gmail.com>
2019-03-16 11:47:31 +0000

    Add root directory to tar

--- dockerfiles/tarball-builder/build.sh
+++ dockerfiles/tarball-builder/build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -ue
 
-readonly WORKDIR="${HOME}/work"
 readonly REPODIR="${HOME}/egison"
 
 {
   git clone https://github.com/egison/egison.git "${REPODIR}"
+  LATEST_TAG=$(git describe --abbrev=0 --tags)
   cd "${REPODIR}"
   cabal update
   cabal install --only-dependencies
   cabal configure --datadir=/usr/lib --datasubdir=egison
   cabal build
-  mkdir -p "${WORKDIR}"{/bin,/lib/egison}
-  cp "${REPODIR}/dist/build/egison/egison" "${WORKDIR}/bin"
-  cp -rf "${REPODIR}/lib" "${WORKDIR}/lib/egison"
-  cp "${REPODIR}/LICENSE" "${WORKDIR}/LICENSE"
-  cp "${REPODIR}/README.md" "${WORKDIR}/README.md"
-  cp "${REPODIR}/THANKS.md" "${WORKDIR}/THANKS.md"
-  tar -zcvf "${REPODIR}.tar.gz" -C "${WORKDIR}" bin lib LICENSE README.md THANKS.md
+  WORKDIR="${HOME}/work"
+  BUILDROOT="${WORKDIR}/egison-${LATEST_TAG}"
+  mkdir -p "${BUILDROOT}"{/bin,/lib/egison}
+  cp "${REPODIR}/dist/build/egison/egison" "${BUILDROOT}/bin"
+  cp -rf "${REPODIR}/lib" "${BUILDROOT}/lib/egison"
+  cp "${REPODIR}/LICENSE" "${BUILDROOT}/LICENSE"
+  cp "${REPODIR}/README.md" "${BUILDROOT}/README.md"
+  cp "${REPODIR}/THANKS.md" "${BUILDROOT}/THANKS.md"
+  tar -zcvf "${REPODIR}.tar.gz" -C "${WORKDIR}" "${BUILDROOT}"
 } >&2
 cat "${HOME}/egison.tar.gz"
