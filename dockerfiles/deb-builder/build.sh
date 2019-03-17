2e70ffad27dab31627b7f1cd22102f611b39f15f
greymd <greengregson@gmail.com>
2019-03-17 00:24:29 +0000

    update

--- dockerfiles/deb-builder/build.sh
+++ dockerfiles/deb-builder/build.sh
@@ -13,6 +13,7 @@ REPO="greymd/rpm-egison"
   cp -rf "/tmp/extract/egison-${VERSION}"/{bin,lib} /tmp/deb-template/
   rm -rf "/tmp/extract"
   bash -x /tmp/changelog.sh
+  sed -i "s/@@@VERSION@@@/${VERSION}/g" /tmp/deb-template/debian/control
   mv /tmp/deb-template "/tmp/egison-${VERSION}"
   cd "/tmp/egison-${VERSION}"
   debuild -sd <<<y
