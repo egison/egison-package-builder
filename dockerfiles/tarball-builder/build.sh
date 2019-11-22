76aa1b183b3d72cd6a2caf0689644e118c47330b
greymd <yamadagrep@gmail.com>
2019-11-23 06:58:26 +0900

    [skip ci] cabal-install 3.0.0 and ghc 8.8.1 are used

--- dockerfiles/tarball-builder/build.sh
+++ dockerfiles/tarball-builder/build.sh
@@ -9,14 +9,19 @@ readonly GCC_OPT='-optl-static'
   git clone -b "${VERSION}" https://github.com/egison/egison.git "${REPODIR}"
   cd "${REPODIR}"
   sed -i "/Executable egison/,\${s/ghc-options.*/& $GCC_OPT/}" egison.cabal
-  cabal update
-  cabal install --only-dependencies
-  cabal configure --datadir=/usr/lib --datasubdir=egison
-  cabal build
+  cabal v2-update
+  cabal v2-install --only-dependencies --lib
+  cabal v2-configure
+  cabal v2-build
+  _pathsfile="$(find "${REPODIR}/dist-newstyle" -type f -name 'Paths_egison.hs' | head -n 1)"
+  perl -i -pe 's@datadir[ ]*=[ ]*.*$@datadir = "/usr/lib/egison"@' "$_pathsfile"
+  cp "$_pathsfile" ./hs-src
+  cabal v2-build
+  _binfile="$(find "${REPODIR}/dist-newstyle" -type f -name 'egison')"
   WORKDIR="${HOME}/work"
   BUILDROOT="${WORKDIR}/egison-${VERSION}"
   mkdir -p "${BUILDROOT}"{/bin,/lib/egison}
-  cp "${REPODIR}/dist/build/egison/egison" "${BUILDROOT}/bin"
+  cp "${_binfile}" "${BUILDROOT}/bin"
   cp -rf "${REPODIR}/lib" "${BUILDROOT}/lib/egison"
   cp "${REPODIR}/LICENSE" "${BUILDROOT}/LICENSE"
   cp "${REPODIR}/README.md" "${BUILDROOT}/README.md"
